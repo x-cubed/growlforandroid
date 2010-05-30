ea68066c6f217d7d82100c3029292353a812fe15
carey.bishop@gmail.com <carey.bishop@gmail.com@2a5df900-0be2-11df-bf12-379292acd595>
2010-05-30 03:51:18 +0000

    - Message decryption works for basic messages - Fixed a cursor leak in GrowlListenerService

--- trunk/GrowlForAndroid/src/com/growlforandroid/gntp/EncryptionType.java
+++ trunk/GrowlForAndroid/src/com/growlforandroid/gntp/EncryptionType.java
@@ -9,15 +9,19 @@ import javax.crypto.*;
 import javax.crypto.spec.*;
 
 public enum EncryptionType {
-	None ("NONE"),
-	AES ("AES"),
-	DES ("DES"),
-	TripleDES ("3DES");
+	None ("NONE", "", 0),
+	AES ("AES", "AES/CBC/PKCS5Padding", 24),
+	DES ("DES", "DES/CBC/PKCS5Padding", 8),
+	TripleDES ("3DES", "DESede/CBC/PKCS5Padding", 24);
 	
 	public final String Type;
+	public final String CipherName;
+	public final int KeyLength;
 	
-	private EncryptionType(String type) {
+	private EncryptionType(String type, String cipherName, int keyLength) {
 		Type = type;
+		CipherName = cipherName;
+		KeyLength = keyLength;
 	}
 	
 	public String toString() {
@@ -30,12 +34,24 @@ public enum EncryptionType {
 		if (this == EncryptionType.None) {
 			return null;
 		}
+		
+		if (key.length < KeyLength) {
+			// Key is too short
+			throw new InvalidKeyException("Key must be at least " + KeyLength + " bytes");
+		}
+		if (key.length > KeyLength) {
+			// Key is too long, just take the first KeyLength bytes
+			byte[] newKey = new byte[KeyLength];
+			for(int i = 0; i<newKey.length; i++) {
+				newKey[i] = key[i];
+			}
+			key = newKey;
+		}
 
 		IvParameterSpec ivSpec = new IvParameterSpec(iv);
 		SecretKeySpec secretKey = new SecretKeySpec(key, Type);
-		
-		String algorithmAndOptions = Type + "/CBC/PKCS5Padding";
-		Cipher cipher = Cipher.getInstance(algorithmAndOptions);
+
+		Cipher cipher = Cipher.getInstance(CipherName);
 		cipher.init(Cipher.DECRYPT_MODE, secretKey, ivSpec);
 		
 		return cipher;
